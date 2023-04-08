8441e8a3f3df7ac56bc62827f9717cfc3518a0de
Esmaeil kazemi <smk182018@gmail.com>
2023-04-08 12:01:17 +0430

    feat(): add router related to authentication

--- routers.py
+++ routers.py
@@ -1,7 +1,9 @@
 from fastapi import APIRouter
 from blog.urls import blog_router
 from user.urls import user_router
+from authentication.urls import router
 api_router = APIRouter()
 
 api_router.include_router(blog_router, prefix="/blog")
 api_router.include_router(user_router, prefix="/user")
+api_router.include_router(router)
