15b2c2bc0727736de30643b84f2e0b7587c051dc
Esmaeil kazemi <smk182018@gmail.com>
2023-05-07 17:22:37 +0430

    fix(): update models base database config

--- blog/models.py
+++ blog/models.py
@@ -4,23 +4,15 @@ from sqlalchemy import (
     String,
     ForeignKey,
 )
-from database import (
-    Base,
-    engine,
-)
+from db.base_class import Base
 from sqlalchemy.orm import relationship
 
 
 
 class Blog(Base):
-    __tablename__ = "blogs"
     id = Column(Integer, primary_key=True, index=True)
     title = Column(String)
     body = Column(String)
-    user_id = Column(Integer, ForeignKey('users.id'))
-
-
+    owner_id = Column(Integer, ForeignKey('user.id'))
     creator = relationship("User", back_populates="blogs")
 
-
-Base.metadata.create_all(engine)
--- user/models.py
+++ user/models.py
@@ -5,17 +5,13 @@ from sqlalchemy import (
     String,
 )
 from sqlalchemy.orm import relationship
-from database import Base, engine
+from db.base_class import Base
 
 
 class User(Base):
-    __tablename__ = "users"
     id = Column(Integer, primary_key=True, index=True)
     email = Column(String, unique=True, index=True)
     hashed_password = Column(String)
     is_active = Column(Boolean, default=True)
-
     blogs = relationship('Blog',  back_populates="creator")
 
-
-Base.metadata.create_all(engine)
