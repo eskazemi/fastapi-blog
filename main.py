6d04fcd34834ea6214c41bb6e71afbecedfed617
Esmaeil kazemi <smk182018@gmail.com>
2023-05-08 08:54:17 +0430

    Feat(): update related to app

--- main.py
+++ main.py
@@ -1,20 +1,33 @@
 from fastapi import FastAPI
 from starlette.middleware.cors import CORSMiddleware
 from routers import api_router
+from db.session import engine
+from db.base_class import Base
 
-app = FastAPI(
-    title="Blog",
-    version="1.0.0"
-)
+from config import get_setting
+
+
+settings = get_setting()
+
+
+
+def create_tables():
+    Base.metadata.create_all(bind=engine)
+
+
+def start_application():
+    app = FastAPI(title=settings.name, version=settings.project_version)
+    create_tables()
+    return app
+
+
+app = start_application()
 app.include_router(api_router)
-
-origins = ["*"]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=settings.origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
