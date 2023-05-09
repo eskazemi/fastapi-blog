537c0f0607493e46471dba814c9b7899b6720d8c
Esmaeil kazemi <smk182018@gmail.com>
2023-05-09 14:47:55 +0430

    fix(): update

--- user/urls.py
+++ user/urls.py
@@ -1,38 +1,26 @@
-from typing import List
 from fastapi import (
     APIRouter,
     Depends,
 )
 from user.schema import (
-    CreateUser,
-    UpdateUser,
-    ShowUser
+    CreateUser
 )
 from authentication.oauth import get_current_user
 from sqlalchemy.orm import Session
-from database import get_db
+from db.session import get_db
 from .repository import UserRepository
 
 user_router = APIRouter(tags=["user"])
 
 
 @user_router.post('', status_code=201)
-def create(body: CreateUser, db: Session = Depends(get_db),
-           get_current_user: CreateUser = Depends(get_current_user)
-           ):
+def create(body: CreateUser, db: Session = Depends(get_db)):
     return UserRepository.create(db, body)
 
-
-@user_router.get('/{id}', response_model=ShowUser)
-def get_one(user_id: int, db: Session = Depends(get_db),
-            get_current_user: CreateUser = Depends(get_current_user)
-            ):
-    return UserRepository.get_one(db, user_id)
-
-
-@user_router.put('/{user_id}', )
-def update(user_id: int, db: Session = Depends(get_db),
-           get_current_user: CreateUser = Depends(get_current_user)
-           ):
-    pass
-
+#
+# @user_router.get('/{id}', response_model=ShowUser)
+# def get_one(user_id: int, db: Session = Depends(get_db),
+#             get_current_user: CreateUser = Depends(get_current_user)
+#             ):
+#     return UserRepository.get_one(db, user_id)
+#
